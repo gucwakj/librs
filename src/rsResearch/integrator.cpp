f221c41c2f94bdb08660e9665ae578cb364261af
Kevin Gucwa <gucwakj@gmail.com>
2015-12-08 15:25:05 -0800

    combine more of the forms output array creation

--- src/rsResearch/integrator.cpp
+++ src/rsResearch/integrator.cpp
@@ -8,10 +8,10 @@
 using namespace rsResearch;
 
 Integrator::Integrator(void) {
-	_body_length = 0;
 	_form = 0;
+	_num_body = 0;
+	_num_legs = 0;
 	_num_robots = 0;
-	_num_vars = 0;
 	_time = 0;
 }
 
@@ -27,10 +27,10 @@ void Integrator::setup(int (*function)(double, const double[], double[], void*),
 	_system = {function, NULL, static_cast<size_t>(params->num_vars), (void *)params};
 	_driver = gsl_odeiv2_driver_alloc_y_new(&_system, gsl_odeiv2_step_rkf45, 1e-4, 1e-4, 0);
 	_array.resize(params->num_vars);
-	_body_length = params->num_body;
+	_num_body = params->num_body;
+	_num_legs = params->num_legs;
+	_num_robots = params->num_robots;
 	_form = params->form;
-	_num_robots = params->num_legs + params->num_body;
-	_num_vars = params->num_vars;
 }
 
 const rs::Vec Integrator::runStep(float newtime) {
@@ -47,21 +47,17 @@ const rs::Vec Integrator::runStep(float newtime) {
 	}
 
 	// save output array
-	if (_form == Forms::Dog) {
-		for (int j = 0, k = 0; j < _num_vars; j+=3, k++) {
-			V[k] = _array[j+1]*cos(_array[j]);
-		}
+	for (int i = 0, j = 0; i < _num_body*3; i+=3, j++) {
+		V[j] = _array[i+1]*cos(_array[i]);
 	}
-	else if (_form == Forms::Salamander) {
-		float theta_up = -5*M_PI/6;
-		float theta_down = -M_PI/6;
-		float a = theta_up - M_PI;
-		float b = (2*M_PI - theta_down + theta_up)/(M_PI);
-		float c = (theta_down - theta_up)/M_PI;
-		for (int i = 0, j = 0; i < _num_vars; i+=3, j++) {
-			if (i < _body_length*3)
-				V[j] = _array[i+1]*cos(_array[i]);
-			else {
+	if (_num_legs) {
+		if (_form == Forms::Salamander) {
+			float theta_up = -5*M_PI/6;
+			float theta_down = -M_PI/6;
+			float a = theta_up - M_PI;
+			float b = (2*M_PI - theta_down + theta_up)/(M_PI);
+			float c = (theta_down - theta_up)/M_PI;
+			for (int i = _num_body*3, j = _num_body; i < _num_robots*3; i+=3, j++) {
 				// get continuous output vectors
 				if (_array[i] < theta_up)
 					V[j] = theta_up + (_array[i] - theta_up)*b;
@@ -69,21 +65,15 @@ const rs::Vec Integrator::runStep(float newtime) {
 					V[j] = theta_up + (_array[i] - theta_up)*c;
 				else
 					V[j] = theta_down + (_array[i] - a)*b;
-				// drop values to start at zero
-				//if (i == 1) init[k - _body_length] = x[k][i];
-				//V[k] -= init[k - _body_length];
 				// flip right side legs for linkbots
-				if ( !((j+1 - _body_length)%2) ) V[j] = -V[j];
+				if ( !((j+1 - _num_body)%2) ) V[j] = -V[j];
 				// scale
 				V[j] *= 4;
 			}
 		}
 	}
-	else if (_form == Forms::Snake) {
-		for (int j = 0, k = 0; j < _num_vars; j+=3, k++) {
-			V[k] = _array[j+1]*cos(_array[j]);
-		}
-	}
+
+	// return output array
 	return V;
 }
 
